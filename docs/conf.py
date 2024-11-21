7d7c304c20a401975646d8bc7a3644e72a88ac33
davidtsukiyama <davidtsukiyama1@gmail.com>
2024-11-21 08:26:17 -0800

    update package

--- docs/conf.py
+++ docs/conf.py
@@ -7,33 +7,28 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os
-import sys
 from importlib import metadata
 
-sys.path.insert(0, os.path.abspath("../.."))
-
-
-project = "model-experiment-and-artifact-management"
-copyright = "2024, David Tsukiyama"
-author = "David Tsukiyama"
-release = "0.0.1"
+project = 'experiment tracking'
+copyright = '2024, David Tsukiyama'
+author = 'David Tsukiyama'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# General configuration
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_autodoc_typehints",
-]
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    ]
 
-templates_path = ["_templates"]
-exclude_patterns = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. experiment tracking documentation master file, created by
+   sphinx-quickstart on Thu Nov 21 07:41:50 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+experiment tracking documentation
+=================================
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   reference/modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
