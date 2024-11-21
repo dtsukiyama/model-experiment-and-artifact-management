a3aab9ddd385d287246e6138e5d733b2190d8962
davidtsukiyama <davidtsukiyama1@gmail.com>
2024-11-21 08:26:34 -0800

    reformatted

--- docs/conf.py
+++ docs/conf.py
@@ -9,26 +9,25 @@
 import os
 from importlib import metadata
 
-project = 'experiment tracking'
-copyright = '2024, David Tsukiyama'
-author = 'David Tsukiyama'
-release = '0.0.1'
+project = "experiment tracking"
+copyright = "2024, David Tsukiyama"
+author = "David Tsukiyama"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    ]
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
